2fff7d8f5c6dae86811308b3c7a40e1d85c09e58
Kerry Sheldon <kerry.sheldon@gmail.com>
2023-12-17 16:06:01 -0700

    rename things

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2021, // or a later ECMAScript version
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true, // If using React
     },
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ["@typescript-eslint", "prettier"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   rules: {
-    // Add your specific rules here
-    'no-console': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'prettier/prettier': 'error',
+    "no-console": "warn",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "prettier/prettier": "error",
   },
 };
--- lib/congressional-app-stack.ts
+++ lib/congressional-app-stack.ts
@@ -1,15 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import { RemovalPolicy } from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import { RemovalPolicy } from "aws-cdk-lib";
 
 export class CongressionalAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'front-end-bucket', {
-      bucketName: 'kjs222-congressional-application',
+    const bucket = new s3.Bucket(this, "front-end-bucket", {
+      bucketName: "kjs222-congressional-application",
       publicReadAccess: true,
       removalPolicy: RemovalPolicy.DESTROY,
       accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
@@ -17,24 +17,23 @@ export class CongressionalAppStack extends cdk.Stack {
         blockPublicAcls: false,
         blockPublicPolicy: false,
         ignorePublicAcls: false,
-        restrictPublicBuckets: false
+        restrictPublicBuckets: false,
       }),
       autoDeleteObjects: true,
       cors: [
         {
-          allowedHeaders: ['*'],
+          allowedHeaders: ["*"],
           allowedMethods: [s3.HttpMethods.GET],
-          allowedOrigins: ['*'],
+          allowedOrigins: ["*"],
           exposedHeaders: [],
         },
       ],
-      websiteIndexDocument: 'index.html',
+      websiteIndexDocument: "index.html",
     });
 
-
-  new s3deploy.BucketDeployment(this, "deployment", {
-      sources: [s3deploy.Source.asset('./congressional-app-frontend/build')],
+    new s3deploy.BucketDeployment(this, "deployment", {
+      sources: [s3deploy.Source.asset("./frontend/build")],
       destinationBucket: bucket,
     });
-}
+  }
 }
