33a20abbf1e44f5f5ac35764e4c79e45dd57c920
Kerry Sheldon <kerry.sheldon@gmail.com>
2024-01-13 18:34:54 -0700

    add api tests

---
+++ backend/test/unit/api/vote-handler.spec.ts
@@ -0,0 +1,67 @@
+import { expect } from "chai";
+import { stub, restore, SinonStub } from "sinon";
+
+import { DynamoAnalyzedVoteRepository } from "../../../src/api/adapters/dynamo-analyzed-vote-repository";
+
+import { handler } from "../../../src/api/handlers/vote-handler";
+
+describe("handler", () => {
+  describe("handler", () => {
+    let getVoteSummaryStub: SinonStub;
+    let getPartyDetailStub: SinonStub;
+    let getStateDetailStub: SinonStub;
+
+    beforeEach(() => {
+      restore();
+      getVoteSummaryStub = stub(
+        DynamoAnalyzedVoteRepository.prototype,
+        "getVoteSummary"
+      );
+      getPartyDetailStub = stub(
+        DynamoAnalyzedVoteRepository.prototype,
+        "getVotePartyDetail"
+      );
+      getStateDetailStub = stub(
+        DynamoAnalyzedVoteRepository.prototype,
+        "getVoteStateDetail"
+      );
+    });
+
+    it("should call getVoteSummary with the correct params when no query params present", async () => {
+      getVoteSummaryStub.resolves(null);
+      await handler({ pathParameters: { id: "123" } });
+      expect(getVoteSummaryStub.calledWith("123")).to.be.true;
+    });
+
+    it("should call getVotePartyDetail with the correct params when query params present", async () => {
+      getVoteSummaryStub.resolves(null);
+      await handler({
+        pathParameters: { id: "123" },
+        queryStringParameters: { party: "democratic" },
+      });
+      expect(getPartyDetailStub.calledWith("123", "democratic")).to.be.true;
+    });
+
+    it("should call getVoteStateDetail with the correct params when query params present", async () => {
+      getVoteSummaryStub.resolves(null);
+      await handler({
+        pathParameters: { id: "123" },
+        queryStringParameters: { state: "CO" },
+      });
+      expect(getStateDetailStub.calledWith("123", "CO")).to.be.true;
+    });
+
+    it("should return the expected response on a successful call", async () => {
+      getVoteSummaryStub.resolves({
+        any: "thing",
+      });
+      const response = await handler({
+        pathParameters: { id: "123" },
+      });
+      expect(response).to.deep.equal({
+        statusCode: 200,
+        body: JSON.stringify({ status: "success", data: { any: "thing" } }),
+      });
+    });
+  });
+});
---
+++ backend/test/unit/api/votes-handler.spec.ts
@@ -0,0 +1,52 @@
+import { expect } from "chai";
+import { stub, restore, SinonStub } from "sinon";
+
+import { DynamoAnalyzedVoteRepository } from "../../../src/api/adapters/dynamo-analyzed-vote-repository";
+
+import { handler } from "../../../src/api/handlers/votes-handler";
+
+describe("handler", () => {
+  describe("handler", () => {
+    let getVotesStub: SinonStub;
+
+    beforeEach(() => {
+      restore();
+      getVotesStub = stub(DynamoAnalyzedVoteRepository.prototype, "getVotes");
+    });
+
+    it("should call getVotes with the correct params when a valid query param present", async () => {
+      getVotesStub.resolves([]);
+      await handler({ queryStringParameters: { chamber: "house" } });
+      expect(getVotesStub.calledWith("house")).to.be.true;
+    });
+
+    it("should return the expected response on a successful call", async () => {
+      getVotesStub.resolves([]);
+      const response = await handler({
+        queryStringParameters: { chamber: "house" },
+      });
+      expect(response).to.deep.equal({
+        statusCode: 200,
+        body: JSON.stringify({ status: "success", data: [] }),
+      });
+    });
+
+    it("should return the expected response on an invalid request without a chamber", async () => {
+      const response = await handler({});
+      expect(response).to.deep.equal({
+        statusCode: 400,
+        body: "Missing chamber",
+      });
+    });
+
+    it("should return the expected response on an invalid request with a bad chamber", async () => {
+      const response = await handler({
+        queryStringParameters: { chamber: "bad" },
+      });
+      expect(response).to.deep.equal({
+        statusCode: 400,
+        body: "Invalid chamber",
+      });
+    });
+  });
+});
