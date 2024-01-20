1f38a614fa766edaf1509dccc2b857312507c836
Kerry Sheldon <kerry.sheldon@gmail.com>
2024-01-20 15:41:30 -0700

    fix tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 test('renders house button', () => {
   render(<App />);
   const linkElements = screen.getAllByRole('link', { name: /house/i });
-  expect(linkElements).toHaveLength(2); // Adjust the expected length based on your actual number of elements.
+  expect(linkElements).toHaveLength(2); 
   linkElements.forEach((linkElement) => {
     expect(linkElement).toBeInTheDocument();
   });
@@ -14,7 +14,7 @@ test('renders house button', () => {
 test('renders senate button', () => {
   render(<App />);
   const linkElements = screen.getAllByRole('link', { name: /senate/i });
-  expect(linkElements).toHaveLength(2); // Adjust the expected length based on your actual number of elements.
+  expect(linkElements).toHaveLength(2);
   linkElements.forEach((linkElement) => {
     expect(linkElement).toBeInTheDocument();
   });
