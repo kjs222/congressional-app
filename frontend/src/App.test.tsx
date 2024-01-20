1179c2db5116a545baf27405f5ef774d43dd13b5
Kerry Sheldon <kerry.sheldon@gmail.com>
2024-01-20 15:41:02 -0700

    add a few frontend tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test.skip('renders house link', () => {
+test('renders house button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/house/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElements = screen.getAllByRole('link', { name: /house/i });
+  expect(linkElements).toHaveLength(2); // Adjust the expected length based on your actual number of elements.
+  linkElements.forEach((linkElement) => {
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+test('renders senate button', () => {
+  render(<App />);
+  const linkElements = screen.getAllByRole('link', { name: /senate/i });
+  expect(linkElements).toHaveLength(2); // Adjust the expected length based on your actual number of elements.
+  linkElements.forEach((linkElement) => {
+    expect(linkElement).toBeInTheDocument();
+  });
 });
